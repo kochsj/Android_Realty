3b280645ef1d53fa23dc76be317308b038c14071
Stephen Koch <kochsj@gmail.com>
2020-05-14 13:13:11 -0700

    style map view

--- app/src/main/java/com/kochsj/realrealty/MainActivity.java
+++ app/src/main/java/com/kochsj/realrealty/MainActivity.java
@@ -1,18 +1,23 @@
 package com.kochsj.realrealty;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         // intent is to render a map...
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, MapsActivity.class);
+//        startActivity(intent);
     }
+
+    ViewPager searchView = findViewById(R.id.center_view);
+
+
 }
