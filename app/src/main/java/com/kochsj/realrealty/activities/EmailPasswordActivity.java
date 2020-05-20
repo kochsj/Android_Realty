21cebee9065cb1191d7fcb17beb61c3dd5dfea86
Stephen Koch <kochsj@gmail.com>
2020-05-20 12:32:02 -0700

    5 icons in bottom nav

--- app/src/main/java/com/kochsj/realrealty/activities/EmailPasswordActivity.java
+++ app/src/main/java/com/kochsj/realrealty/activities/EmailPasswordActivity.java
@@ -45,7 +45,7 @@ public class EmailPasswordActivity extends BaseActivity implements View.OnClickL
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_profile, R.id.navigation_chat, R.id.navigation_more)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
