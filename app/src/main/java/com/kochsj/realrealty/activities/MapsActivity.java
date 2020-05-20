30141225647737df9dea7c9557ba77e132dab341
Stephen Koch <kochsj@gmail.com>
2020-05-19 19:20:17 -0700

    add bottom nav bar

--- app/src/main/java/com/kochsj/realrealty/activities/MapsActivity.java
+++ app/src/main/java/com/kochsj/realrealty/activities/MapsActivity.java
@@ -23,8 +23,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.center_view).setVisibility(View.GONE);
+//        setContentView(R.layout.activity_main);
+//        findViewById(R.id.center_view).setVisibility(View.GONE);
+        setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
