a8ddbb083737f1480585b056c00f875cd522c3a6
Stephen Koch <kochsj@gmail.com>
2020-05-20 12:09:11 -0700

    create map fragment and model

---
+++ app/src/main/java/com/kochsj/realrealty/ui/search/MapFragment.java
@@ -0,0 +1,38 @@
+package com.kochsj.realrealty.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+
+import com.kochsj.realrealty.R;
+
+public class MapFragment extends Fragment {
+
+    private MapViewModel mapViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mapViewModel = new ViewModelProvider((ViewModelStoreOwner) this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+
+        mapViewModel.getText().observe((LifecycleOwner) this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        return root;
+    }
+}
